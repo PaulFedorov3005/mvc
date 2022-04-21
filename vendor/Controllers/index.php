9da992a07003c838c9284899e622fd68746c61f2
PaulFedorov3005 <paulfedorovster@gmail.com>
2022-04-21 18:32:23 +0300

    controller index add create metod

--- vendor/Controllers/index.php
+++ vendor/Controllers/index.php
@@ -7,5 +7,8 @@ class index
     public function index(){
         echo 'index controller index action';
     }
+    public function cteate(){
+        echo 'index controller index create';
+    }
 
 }
