81e53160e07f74406e91e78ffcbbf07ef104d275
PaulFedorov3005 <paulfedorovster@gmail.com>
2022-04-23 11:00:35 +0300

    controller index remove un usable

--- vendor/Controllers/index.php
+++ vendor/Controllers/index.php
@@ -7,9 +7,4 @@ class index
     public function index(){
         echo 'index controller index action';
     }
-    public function cteate(){
-        echo 'index controller index create';
-    }
-    public function store()
-        echo 'index controller index store method';
 }
