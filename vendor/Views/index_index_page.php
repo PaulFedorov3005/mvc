51c3fca460b177d32a1cc8db40e76c3276415f8c
PaulFedorov3005 <paulfedorovster@gmail.com>
2022-04-25 11:54:39 +0300

    view class

---
+++ vendor/Views/index_index_page.php
@@ -0,0 +1,7 @@
+<h2>Home</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam architecto assumenda cumque cupiditate
+    dolor dolore dolorum eligendi eum fuga hic impedit inventore labore laudantium maxime odit perferendis quae, quasi
+    quibusdam quidem, quisquam quo quod sapiente soluta vero! Earum error est expedita, fugiat iste pariatur praesentium
+    repellat sapiente similique vero. Aut dolorum nesciunt optio pariatur placeat repudiandae similique vel. Adipisci,
+    laudantium, molestiae. Adipisci amet animi consequatur dignissimos ducimus et fuga iste omnis suscipit ullam. Autem
+    culpa earum illum quisquam velit?</p>
