855894f009594774cae8285973813828d26f992a
PaulFedorov3005 <paulfedorovster@gmail.com>
2022-04-28 14:57:26 +0300

    Controller index and note

--- vendor/Controllers/Index.php
+++ vendor/Controllers/Index.php
@@ -4,14 +4,9 @@ namespace Controllers;
 
 use Core\View;
 
-class index
+class index extends AbstractController
 {
-    protected $view;
 
-    public function __construct()
-    {
-        $this->view = new View();
-    }
 
     public function index(){
         $this->view->render('index_index_page');
---
+++ vendor/Controllers/Note.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Controllers;
+
+use Core\View;
+
+class Note extends AbstractController
+{
+    public function index()
+    {
+        // TODO: Implement index() method.
+    }
+
+ }
